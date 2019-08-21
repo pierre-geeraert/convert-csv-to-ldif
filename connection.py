9429a38d2a8641b7c13027bdbecbbfd34f1eec4f
pierre geeraert <pierre.geeraert@viacesi.fr>
2019-08-21 11:30:33 +0100

    5d42f1f1fb3e614f98c57875 connection ldap work now

--- connection.py
+++ connection.py
@@ -2,23 +2,9 @@ import ldap3
 from ldap3 import Server, Connection, ALL
 
 
-if __name__ == "__main__":
-    ldap_server="geeraert.eu"
-    username = "cn=admin,dc=geeraert,dc=eu"
-    password= "dinner_sl@v3"
-    # the following is the user_dn format provided by the ldap server
-    user_dn = "uid="+username+",ou=someou,dc=somedc,dc=local"
-    # adjust this to your base dn for searching
-    base_dn = "dc=somedc,dc=local"
-    connect = ldap3.open(ldap_server)
-    search_filter = "uid="+username
-    try:
-        #if authentication successful, get the full user data
-        connect.bind_s(user_dn,password)
-        result = connect.search_s(base_dn,ldap3.SCOPE_SUBTREE,search_filter)
-        # return all user data results
-        connect.unbind_s()
-        print(result)
-    except ldap3.LDAPError:
-        connect.unbind_s()
-print("authentication error")
+
+### usefull help: https://ldap3.readthedocs.io/tutorial.html
+conn = Connection(server, 'cn=admin,dc=geeraert,dc=eu', '', auto_bind=True)
+print(conn.extend.standard.who_am_i())
+conn.search('ou=cesi,dc=geeraert,dc=eu', '(objectclass=person)')
+print(conn.entries)
