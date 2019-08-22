2d0e25189585bea704da6c1fdfa995ffff7e1fb7
pi <pi@pi.fr>
2019-08-22 17:49:12 +0100

    connection working after fix

--- connection.py
+++ connection.py
@@ -1,10 +1,15 @@
-import ldap3
-from ldap3 import Server, Connection, ALL
+import json
+from ldap3 import Server, Connection, ALL, NTLM
 
+PathCredential_ldap = "ignore/ldap_credential.json"
 
+def readCredential(PathCredential_ldap, type):
+    with open(PathCredential_ldap) as f:
+        data = json.load(f)
+    return data[type]
 
 ### usefull help: https://ldap3.readthedocs.io/tutorial.html
-conn = Connection(server, 'cn=admin,dc=geeraert,dc=eu', '', auto_bind=True)
+
+server = Server(readCredential(PathCredential_ldap,"server"), get_info=ALL)
+conn = Connection(server, readCredential(PathCredential_ldap,"id"), readCredential(PathCredential_ldap,"password"), auto_bind=True)
 print(conn.extend.standard.who_am_i())
-conn.search('ou=cesi,dc=geeraert,dc=eu', '(objectclass=person)')
-print(conn.entries)
