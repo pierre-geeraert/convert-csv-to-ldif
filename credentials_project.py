b543224ee617ee83e158cb8eafbed6016d0e29a8
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2020-09-23 17:14:55 +0200

    add credentials file

---
+++ credentials_project.py
@@ -0,0 +1,8 @@
+class ldap:
+    id_ldap = "cn=admin,dc=server,dc=com"
+    password_ldap = "password"
+    server_ldap = "server.comeu"
+    baseDN_ldap = "dc=server,dc=com"
+
+class pushbullet:
+    token_pushbullet = "token_push"
